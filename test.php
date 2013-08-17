8cdd752c074efd854de5dab3d42e1beed022561b
concise <concise_net@163.com>
2013-08-17 16:48:21 +0800

    this->db

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
 	class aa{
+		private $db;
 		function __construct(){
-		
+			$this->db=M("user");
 		}
 		function index(){
-			
+			$this->db->where("id=1")->select();
 		}
 
 		function add(){
